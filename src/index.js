d6b782b1d1469d76be08a78b9f8abd5ddd1d4850
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-02-04 22:04:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-// Get the root div
-const rootElement = document.getElementById("root");
+
 
 // Create root and render
 const root = ReactDOM.createRoot(rootElement);
