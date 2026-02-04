8e280aeb86e90598427b61cfcecc5731ec2f4dd9
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-02-04 21:45:06 +0530

    Refactor ReactDOM rendering to use createRoot

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+// Get the root div
+const rootElement = document.getElementById("root");
+
+// Create root and render
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
